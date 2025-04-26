f1118ee1b42ededd9f5c84c47b104f6e230e3c32
Saba Kandashvili <sabakandashvili2004@gmail.com>
2025-04-26 12:57:12 +0400

    something messed up initial commit

---
+++ src/main/java/com/aughma/nacarqeqia/NacarqeqiaApplication.java
@@ -0,0 +1,13 @@
+package com.aughma.nacarqeqia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacarqeqiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacarqeqiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aughma/nacarqeqia/NacarqeqiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aughma.nacarqeqia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacarqeqiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
