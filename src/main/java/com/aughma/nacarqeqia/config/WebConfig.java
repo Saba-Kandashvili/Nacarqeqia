aecdb52ab89a3daf2fc23e508a593acaf5e6cf62
Saba Kandashvili <sabakandashvili2004@gmail.com>
2025-05-24 23:22:32 +0400

    multilanguage thing is somewhat working

--- src/main/java/com/aughma/nacarqeqia/config/WebConfig.java
+++ src/main/java/com/aughma/nacarqeqia/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.aughma.nacarqeqia.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,6 +27,14 @@ public class WebConfig implements WebMvcConfigurer {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         return localeChangeInterceptor;
+    }
+    
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 
     @Override
